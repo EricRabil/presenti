db390cde1cd264100795c686b68eff1e01a528f3
Eric Rabil <ericjrabil@gmail.com>
2020-04-20 21:09:48 -0400

    Removed compiled code

--- packages/PresenceBot/js/scoped-adapter.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const remote_presence_utils_1 = require("remote-presence-utils");
-class ScopedPresenceAdapter extends remote_presence_utils_1.PresenceAdapter {
-}
-exports.ScopedPresenceAdapter = ScopedPresenceAdapter;
--- packages/PresenceBot/js/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import { HttpResponse } from "uWebSockets.js";
-export declare function readRequest(res: HttpResponse): Promise<any>;
-export declare namespace PresentiKit {
-    function generatePalette(imageURL: string): Promise<string[]>;
-}
