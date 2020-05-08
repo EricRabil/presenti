6562209912af54dd215587347e50e6057ce1401d
Eric Rabil <ericjrabil@gmail.com>
2020-05-08 00:25:35 -0400

    Added a function to filter out empty values from an object

--- packages/server/src/utils/object.ts
+++ packages/server/src/utils/object.ts
@@ -12,6 +12,12 @@ export function blackHat<T, U = any>(defaultValue: T): Record<keyof U, T> {
   });
 }
 
+export function removeEmptyFields<T extends object>(object: T): Partial<T> {
+  object = Object.assign({}, object);
+  Object.keys(object).forEach(key => (object[key] === undefined || (typeof object[key] === "string" && object[key].length === 0)) && delete object[key]);
+  return object;
+}
+
 export function observeObject<T extends object>(object: T, updated: () => any): T {
   return new Proxy(object, {
     get(target: T, prop: keyof T, receiver: Function) {
