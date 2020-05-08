f0a8fcecc732177500c5f850aef9e127a54da107
Eric Rabil <ericjrabil@gmail.com>
2020-05-07 23:11:40 -0400

    Changed entrypoint for the renderer

--- packages/server/src/Constants.ts
+++ packages/server/src/Constants.ts
@@ -2,4 +2,4 @@ import path from "path";
 
 export const VIEWS_DIRECTORY = path.resolve(__dirname, "..", "views");
 export const STATIC_DIRECTORY = path.resolve(__dirname, "..", "assets");
-export const PRESENTI_ASSET_DIRECTORY = path.resolve(require.resolve("@presenti/renderer"), "..", "..");
+export const PRESENTI_ASSET_DIRECTORY = path.resolve(require.resolve("@presenti/renderer"), "..");
