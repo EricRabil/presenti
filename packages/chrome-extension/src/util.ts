ba7c6fcf78f9d8811ba90079b45a0c29e7ffa941
Eric Rabil <ericjrabil@gmail.com>
2020-05-07 20:55:01 -0400

    Deleted chrome extension for now

--- packages/chrome-extension/src/util.ts
+++
@@ -1,7 +0,0 @@
-export function debounce (fn: Function, wait = 1) {
-  let timeout
-  return function (...args) {
-		if (timeout) return;
-    timeout = setTimeout(() => {fn.call(this, ...args); timeout = null;}, wait)
-  }
-}
