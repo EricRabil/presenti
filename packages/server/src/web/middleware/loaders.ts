399ba88f2c6d5389445bdec48e84c74c27c50a17
Eric Rabil <ericjrabil@gmail.com>
2020-05-08 00:26:51 -0400

    Refactored the logic in UserLoader to be less query-heavy

--- packages/server/src/web/middleware/loaders.ts
+++ packages/server/src/web/middleware/loaders.ts
@@ -8,14 +8,10 @@ import { SharedPresenceService } from "../..";
  * @param includeAuth should the authorization header be used to determine identity?
  */
 export const UserLoader: (includeAuthorization?: boolean) => RequestHandler = includeAuth => async (req, res, next) => {
-  if (req.cookie('identity')) {
-    const user = await User.userForToken(req.cookie('identity')!) as User;
-    if (!includeAuth && !(user instanceof User)) {
-      return next();
-    }
-    res.user = await User.userForToken(req.cookie('identity')!) as User;
-  }
-  else if (includeAuth && req.getHeader('authorization')) res.user = await SecurityKit.validateApiKey(req.getHeader('authorization')) as User;
+  const identity = req.cookie('identity'), authorization = (includeAuth ? req.getHeader('authorization') : null);
+  if (!identity && !authorization) return next();
+  res.user = authorization ? await SecurityKit.validateApiKey(authorization) : identity ? await User.userForToken(identity) : null;
+  
   next();
 }
 
