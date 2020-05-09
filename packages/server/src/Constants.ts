65e455e741a4255b2dd570ab8029f7f660fc31fc
Eric Rabil <ericjrabil@gmail.com>
2020-05-09 01:27:42 -0400

    Add fallback for PRESENTI_ASSET_DIRECTORY in the event @presenti/renderer is not compiled, allowing the server to run without the renderer.
    Any renderer-related functionality will 404 as the related files will be inaccessible.

--- packages/server/src/Constants.ts
+++ packages/server/src/Constants.ts
@@ -3,6 +3,14 @@ import { APIError } from "@presenti/web";
 
 export const VIEWS_DIRECTORY = path.resolve(__dirname, "..", "views");
 export const STATIC_DIRECTORY = path.resolve(__dirname, "..", "assets");
-export const PRESENTI_ASSET_DIRECTORY = path.resolve(require.resolve("@presenti/renderer"), "..");
+
+try {
+  var resolved = path.resolve(require.resolve("@presenti/renderer"), "..");
+} catch {
+  // fallback - this will make p-assets 404, but you can run the server without building a renderer
+  resolved = STATIC_DIRECTORY;
+}
+
+export const PRESENTI_ASSET_DIRECTORY = resolved;
 
 export const MALFORMED_BODY = APIError.badRequest("Malformed body.");
