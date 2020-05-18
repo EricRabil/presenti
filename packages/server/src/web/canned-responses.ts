256667de2a63a1384e4b6054ac11a7b799389086
Eric Rabil <ericjrabil@gmail.com>
2020-05-18 16:04:56 -0400

    Removed remaining pug frontend code

--- packages/server/src/web/canned-responses.ts
+++ packages/server/src/web/canned-responses.ts
@@ -1,10 +1,5 @@
 import { PBResponse, APIError } from "@presenti/web";
 
-/** Renders a 404 error page */
-export function notFound(res: PBResponse) {
-  res.writeStatus(404).render('error', { error: "The resource requested could not be found." });
-}
-
 /** Returns a 404 error response */
 export function notFoundAPI(res: PBResponse) {
   res.json(APIError.notFound("Unknown endpoint."));
--- packages/server/src/web/routes/frontend.ts
+++ packages/server/src/web/routes/frontend.ts
@@ -1,11 +1,10 @@
-import { Any, BodyParser, Get, PBRequest, PBResponse, Post, RequestHandler, RouteData, RouteDataShell, Options } from "@presenti/web";
+import { Any, BodyParser, Get, PBRequest, PBResponse, Post, RequestHandler, RouteData, RouteDataShell, Options, APIError } from "@presenti/web";
 import fs from "fs-extra";
 import path from "path";
 import { TemplatedApp } from "uWebSockets.js";
 import { User } from "../../database/entities";
 import { CONFIG } from "../../utils/config";
 import PBRestAPIBase, { API, GlobalGuards } from "../../structs/rest-api-base";
-import { notFound } from "../canned-responses";
 import { PRESENTI_ASSET_DIRECTORY, STATIC_DIRECTORY, VIEWS_DIRECTORY } from "../../Constants";
 import { UserLoader, OAuthLoader } from "../middleware/loaders";
 import { IdentityGuardFrontend } from "../middleware/guards";
@@ -20,121 +19,12 @@ export default class Frontend extends PBRestAPIBase {
     super.loadRoutes();
 
     this.app.any('/*', this.buildHandler(RouteDataShell("/*"), (req, res) => {
-      notFound(res);
+      res.json(APIError.notFound())
     }));
   }
 
   buildStack(metadata: RouteData, middleware: RequestHandler[], headers: string[] = []) {
     return super.buildStack(metadata, [UserLoader()].concat(middleware), headers.concat('authorization'));
-  }
-
-  /** Renders the login page */
-  @Get("/login")
-  loginView(req: PBRequest, res: PBResponse) {
-    res.render('login', { signup: CONFIG.registration });
-  }
-
-  /** Renders the signup page, if registration is enabled */
-  @Get("/signup")
-  signupView(req: PBRequest, res: PBResponse) {
-    if (!CONFIG.registration) return notFound(res);
-    res.render('signup');
-  }
-
-  /** Renders the change password page, if the user is signed in */
-  @Get("/changepw", IdentityGuardFrontend)
-  changePassword(req: PBRequest, res: PBResponse) {
-    res.render('changepw');
-  }
-
-  /** Called upon change password form submission, accepts "password" and "newPassword" in the form body */
-  @Post("/changepw", IdentityGuardFrontend, BodyParser)
-  async changePasswordComplete(req: PBRequest, res: PBResponse) {
-    const fail = (msg: string) => res.render('changepw', { error: msg });
-    if (!req.body || !req.body.password || !req.body.newPassword) {
-      return fail('Please fill out all required fields.');
-    }
-
-    const { password, newPassword } = req.body;
-    if (!await res.user!.checkPassword(password)) {
-      return fail('Please enter the correct old password.');
-    }
-
-    await res.user!.setPassword(newPassword);
-    await res.user!.save();
-    
-    res.render('changepw', { message: 'Your password has been changed. All existing tokens have been invalidated.' });
-  }
-
-  /** Called upon signup form submission, accepts "id" and "password" in the form body */
-  @Post("/signup", BodyParser)
-  async signupComplete(req: PBRequest, res: PBResponse) {
-    if (!CONFIG.registration) return notFound(res);
-    const fail = (msg: string) => res.render('signup', { error: msg });
-    if (!req.body || !req.body.id || !req.body.password) {
-      return fail('Please fill out all required fields.');
-    }
-
-    const { id: userID, password } = req.body;
-    let user = await User.findOne({ userID });
-    if (user) return fail("A user with that ID already exists. Please select a different one.");
-
-    user = await User.createUser(userID, password);
-    await user.save();
-
-    const token = await user.token(password);
-    if (!token) return fail("Sorry, we couldn't finish logging you in.");
-
-    res.setCookie('identity', token, { httpOnly: true });
-    res.redirect('/');
-  }
-
-  /** Called to sign out the user */
-  @Get("/logout")
-  logout(req: PBRequest, res: PBResponse) {
-    res.setCookie('identity', '', { maxAge: 0 });
-    res.redirect('/');
-  }
-
-  /** Called upon login form submission, accepts "id" and "password" in form submission */
-  @Post("/login", BodyParser)
-  async loginComplete(req: PBRequest, res: PBResponse) {
-    const fail = () => res.status(401).render('login', { error: 'Invalid credentials.' });
-    if (!req.body || !req.body.id || !req.body.password) {
-      return fail();
-    }
-
-    const { id: userID, password } = req.body;
-    const user = await User.findOne({ userID });
-    if (!user) return fail();
-
-    const token = await user.token(password);
-    if (!token) return fail();
-
-    const redirect = req.cookie('redirect');
-    res.setCookie('identity', token, { httpOnly: true });
-    res.setCookie('redirect', '', { maxAge: 0 });
-
-    console.log(redirect);
-
-    res.redirect(redirect || '/');
-  }
-
-  /** Renders the panel if signed in, and the login page otherwise */
-  @Any("/")
-  rootHandler(req: PBRequest, res: PBResponse) {
-    if (!res.user) {
-      res.redirect('/login');
-      return;
-    }
-
-    res.redirect('/panel');
-  }
-
-  /** Renders the panel home page */
-  @Get("/panel", IdentityGuardFrontend, OAuthLoader)
-  panelView(req: PBRequest, res: PBResponse) {
-    res.render('panel');
   }
 
   /** Serves assets for the presenti renderer */
@@ -144,7 +34,7 @@ export default class Frontend extends PBRestAPIBase {
     const absolute = await Frontend.resolvePresenti(relative).catch(e => null);
     
     if (!absolute || !await fs.pathExists(absolute).catch(e => false) || !await fs.stat(absolute).then(stat => stat.isFile()).catch(e => false)) {
-      return notFound(res);
+      return res.json(APIError.notFound());
     }
 
     await res.file(absolute);
@@ -169,7 +59,7 @@ export default class Frontend extends PBRestAPIBase {
     const absolute = Frontend.resolveStatic(relative);
 
     if (!absolute || !await fs.pathExists(absolute) || !await fs.stat(absolute).then(stat => stat.isFile())) {
-      return notFound(res);
+      return res.json(APIError.notFound());
     }
 
     await res.file(absolute);
@@ -192,12 +82,6 @@ export default class Frontend extends PBRestAPIBase {
   static async resolvePresenti(file: string) {
     let resolved = path.resolve(PRESENTI_ASSET_DIRECTORY, file);
     if (!resolved.startsWith(PRESENTI_ASSET_DIRECTORY)) return null;
-    let [ name, subdir ] = resolved.split('/').reverse();
-    if (subdir !== 'js' && subdir !== 'css') return null;
-    const contents = await fs.readdir(path.resolve(PRESENTI_ASSET_DIRECTORY, subdir));
-    const [ prefix ] = name.split('.');
-    name = contents.find(c => c.split('.')[0] === prefix)!;
-    if (!name) return null;
-    return path.resolve(PRESENTI_ASSET_DIRECTORY, subdir, name);
+    return resolved;
   }
 }
