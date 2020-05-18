65d39b0d5ec440ab07aa0abb8465e42a5f460bea
Eric Rabil <ericjrabil@gmail.com>
2020-05-18 16:03:19 -0400

    Added component for b-checkbox with intermediate value

---
+++ packages/frontend/src/components/inputs/BCheckbox+ThirdState.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-checkbox :indeterminate="state === 1" :value="state === 1 ? null : state === 2" @input="nextState" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class BCheckboxThirdState extends Vue {
+  @Prop({ default: null })
+  value: boolean | null;
+
+  state = this.value === null ? 1 : this.value === true ? 2 : 0;
+
+  nextState() {
+    if ((this.state + 1) === 3) {
+      this.state = 0;
+    } else this.state++;
+
+    this.$emit('input', this.newValue);
+  }
+
+  get newValue() {
+    return this.state === 1 ? null : this.state === 2;
+  }
+}
+</script>
