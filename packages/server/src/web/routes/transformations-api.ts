f57a872c9b088c8bc551b676e7c51c50ec1e6ede
Eric Rabil <ericjrabil@gmail.com>
2020-07-11 22:48:53 -0400

    AJV validation is now a part of the @presenti/web toolkit

--- packages/server/src/web/routes/transformations-api.ts
+++ packages/server/src/web/routes/transformations-api.ts
@@ -1,5 +1,4 @@
-import Ajv from "ajv";
-import { Get, PBRequest, PBResponse, Patch, BodyParser, APIError, Delete, Post, RequestHandler } from "@presenti/web";
+import { Get, PBRequest, PBResponse, Patch, BodyParser, APIError, Delete, Post, RequestHandler, AJVGuard } from "@presenti/web";
 import PBRestAPIBase from "../../structs/rest-api-base";
 import { API, GlobalGuards } from "@presenti/modules";
 import { DenyFirstPartyGuard, IdentityGuard } from "@presenti/web";
@@ -13,84 +12,52 @@ import { TransformationsAPI } from "../../api/transformations";
  * DELETE /api/transformations/:uuid – Deletes the transformation with the given ID
  */
 
-const validator = new Ajv({
-  allErrors: true,
-  useDefaults: true,
-  verbose: false
-});
-
 const uuidSchema = {
   type: 'string',
   format: 'uuid'
 };
 
-/**
- * Accepts a handler function that will be expected to return true if valid, false if invalid, or a string with a custom error message.
- * @param handler validator function to be called
- */
-function Validator(handler: (req: PBRequest) => boolean | string): RequestHandler {
-  return function (req, res, next) {
-    const result = handler(req);
-    if (result === true) return next();
-    else {
-      res.json(APIError.badRequest(typeof result === "string" ? result : undefined));
-      next(true);
-    }
-  }
-}
-
-/**
- * Returns a composable middleware that passes the given schema to the ajv validator
- * @param schema validation schema, passed straight to ajv
- * @param message message to show on error, or the generic bad request message.
- */
-function AJVValidator(schema: object | string | boolean, message?: string) {
-  return function(extractor: (req: PBRequest) => string): RequestHandler {
-    return Validator(req => !validator.validate(schema, extractor(req)) ? (message || false) : true);
-  }
-}
-
-const UUIDValidator = AJVValidator(uuidSchema, "Invalid UUID.");
+const UUIDValidator = AJVGuard(uuidSchema, "Invalid UUID.")(req => req.getParameter(0));
 
 @API("/api/transformations")
 @GlobalGuards(UserLoader(true), IdentityGuard, DenyFirstPartyGuard)
 export class RESTTransformationsAPI extends PBRestAPIBase {
   @Get()
   async lookupTransformations(req: PBRequest, res: PBResponse) {
-    const transformations = await TransformationsAPI.transformations(res.user!.uuid);
+    const transformations = await TransformationsAPI.transformations(res.user.uuid);
 
     res.json({ transformations });
   }
 
   @Post(BodyParser)
   async createTransformation(req: PBRequest, res: PBResponse) {
-    const { rule, ids } = req.body, userUUID = res.user!.uuid;
+    const { rule, ids } = req.body, userUUID = res.user.uuid;
 
     const result = await TransformationsAPI.createTransformation(userUUID, { rule, ids });
 
     res.json(result);
   }
 
-  @Patch("/:uuid", BodyParser, UUIDValidator(req => req.getParameter(0)))
+  @Patch("/:uuid", BodyParser, UUIDValidator)
   async updateTransformation(req: PBRequest, res: PBResponse) {
     const { rule, ids } = req.body, uuid = req.getParameter(0);
 
     const ownerUUID = await TransformationsAPI.authorUUIDForTransformation(uuid);
     if (ownerUUID instanceof APIError) return res.json(ownerUUID);
-    if (ownerUUID !== res.user!.uuid) return res.json(APIError.forbidden("You do not own this transformation."));
+    if (ownerUUID !== res.user.uuid) return res.json(APIError.forbidden("You do not own this transformation."));
 
     const result = await TransformationsAPI.updateTransformation(uuid, { rule, ids });
 
     res.json(result);
   }
 
-  @Delete("/:uuid", UUIDValidator(req => req.getParameter(0)))
+  @Delete("/:uuid", UUIDValidator)
   async deleteTransformation(req: PBRequest, res: PBResponse) {
     const uuid = req.getParameter(0);
 
     const ownerUUID = await TransformationsAPI.authorUUIDForTransformation(uuid);
     if (ownerUUID instanceof APIError) return res.json(ownerUUID);
-    if (ownerUUID !== res.user!.uuid) return res.json(APIError.forbidden("You do not own this transformation."));
+    if (ownerUUID !== res.user.uuid) return res.json(APIError.forbidden("You do not own this transformation."));
 
     const result = await TransformationsAPI.deleteTransformationRule(uuid);
 
--- packages/web/src/guards.ts
+++ packages/web/src/guards.ts
@@ -1,5 +1,6 @@
+import ajv from "ajv";
 import { FIRST_PARTY_SCOPE } from "@presenti/utils";
-import { RequestHandler, APIError } from "./utils";
+import { RequestHandler, APIError, PBRequest } from "./utils";
 
 /** Returns a 401 if the request is not authenticated */
 export const IdentityGuard: RequestHandler = async (req, res, next) => {
@@ -17,4 +18,36 @@ export const DenyFirstPartyGuard: RequestHandler = async (req, res, next) => {
       return next(true);
     }
     next();
+}
+
+/**
+ * Accepts a handler function that will be expected to return true if valid, false if invalid, or a string with a custom error message.
+ * @param handler validator function to be called
+ */
+export function Validator(handler: (req: PBRequest) => boolean | string): RequestHandler {
+  return function (req, res, next) {
+    const result = handler(req);
+    if (result === true) return next();
+    else {
+      res.json(APIError.badRequest(typeof result === "string" ? result : undefined));
+      next(true);
+    }
+  }
+}
+
+/**
+ * Returns a composable middleware that passes the given schema to the ajv validator
+ * @param schema validation schema, passed straight to ajv
+ * @param message message to show on error, or the generic bad request message.
+ */
+export function AJVGuard(schema: object | string | boolean, message?: string) {
+  return function(extractor: (req: PBRequest) => any): RequestHandler {
+    const validator = new ajv({
+      allErrors: true,
+      useDefaults: true,
+      verbose: false
+    });
+
+    return Validator(req => !validator.validate(schema, extractor(req)) ? (message || validator.errorsText(schema["errors"])) : true);
+  }
 }
